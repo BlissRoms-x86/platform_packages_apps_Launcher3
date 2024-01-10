79a75367fbd1e8815a56e0d82cacf938b8885336
hmtheboy154 <buingoc67@gmail.com>
2024-01-10 00:50:38 -0500

    Revert "Allow enabling recents overview grid on phones"
    This reverts commit 6f3ed7652de89f976f4ad7202e4f240a24c1ab41.

--- quickstep/src/com/android/quickstep/views/RecentsView.java
+++ quickstep/src/com/android/quickstep/views/RecentsView.java
@@ -3268,9 +3268,7 @@ public abstract class RecentsView<ACTIVITY_TYPE extends StatefulActivity<STATE_T
             return;
         }
         mActionsView.setSplitButtonVisible(
-                mActivity.getDeviceProfile().overviewShowAsGrid &&
-                        mActivity.getDeviceProfile().isTablet &&
-                        getTaskViewCount() > 1);
+                mActivity.getDeviceProfile().overviewShowAsGrid && getTaskViewCount() > 1);
     }
 
     /**
